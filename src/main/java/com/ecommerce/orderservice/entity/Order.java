1aa9eea01ada6976422bfc62541694f9e1887979
Tulajaram12 <96332761+Tulajaram12@users.noreply.github.com>
2026-09-03 22:54:43 +0530

    Create Order.java

---
+++ src/main/java/com/ecommerce/orderservice/entity/Order.java
@@ -0,0 +1,90 @@
+package com.ecommerce.orderservice.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long productId;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private BigDecimal total;
+
+    @Column(nullable = false)
+    private String status;
+
+    public Order() {
+    }
+
+    public Order(
+            Long userId,
+            Long productId,
+            Integer quantity,
+            BigDecimal total,
+            String status) {
+
+        this.userId = userId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.total = total;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
